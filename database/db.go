3aff55bf436ee2ff560604452cba82642f84fd42
sultanmuhammad <sultan.muhammaddhavi@cbncloud.co.id>
2026-03-02 11:46:17 +0700

    feat: optimize database connection pool

--- database/db.go
+++ database/db.go
@@ -1,14 +1,39 @@
 package database
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
+
+func getEnvInt(key string, defaultValue int) int {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	intVal, err := strconv.Atoi(value)
+	if err != nil {
+		return defaultValue
+	}
+	return intVal
+}
+
+func configureConnectionPool(sqlDB *sql.DB) {
+	maxOpenConns := getEnvInt("DB_MAX_OPEN_CONNS", 10)
+	maxIdleConns := getEnvInt("DB_MAX_IDLE_CONNS", 5)
+	connMaxLifetime := getEnvInt("DB_CONN_MAX_LIFETIME", 3600)
+
+	sqlDB.SetMaxOpenConns(maxOpenConns)
+	sqlDB.SetMaxIdleConns(maxIdleConns)
+	sqlDB.SetConnMaxLifetime(time.Duration(connMaxLifetime) * time.Second)
+}
 
 func Connect() {
 	host := os.Getenv("DB_HOST")
@@ -33,22 +58,25 @@ func Connect() {
 		name = "masakapadb"
 	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=10s&readTimeout=10s&writeTimeout=10s",
 		user, pass, host, port, name,
 	)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic(err)
+		panic("Failed to connect to database: " + err.Error())
 	}
 
 	sqlDB, err := db.DB()
 	if err != nil {
-		panic(err)
+		panic("Failed to get database instance: " + err.Error())
 	}
 
+	configureConnectionPool(sqlDB)
+
 	if err := sqlDB.Ping(); err != nil {
-		panic(err)
+		panic("Failed to ping database: " + err.Error())
 	}
 
 	DB = db
